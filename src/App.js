afdbe3fb9879eb6d868c1c54fa05c061a1a2621e
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-11-11 21:49:46 -0300

    primeira parte

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import Button from './Button';
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = { stateIndex: 0 };
+  }
+
+    handleClick = () => {
+      const lastIndex = pokemons.length - 1;
+      const { stateIndex } = this.state;
+      if (stateIndex < lastIndex) {
+      this.setState((previousState, _props) => ({
+        stateIndex: previousState.stateIndex + 1,
+      }));
+      } else {
+      this.setState(() => ( { stateIndex: 0 }));
+      }
+    }
+
   render() {
+    const stateIndex = this.state.stateIndex;
+    const pokemon = pokemons.find((pokemon, index) => index === stateIndex);
     return (
       <div className="App">
         <h1> Pokedex </h1>
-        <Pokedex pokemons={pokemons} />
+        <Button buttonText=">>>" handleClick={this.handleClick} />
+        <Pokedex pokemon={pokemon} />
       </div>
     );
   }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,17 +2,11 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    constructor(props) {
-        super();
-        this.state = { stateIndex: 0 };
-      } 
-
     render() {
-        const stateIndex = this.state.stateIndex;
-        const pokemon = this.props.pokemons.find((pokemon, index) => index === stateIndex);
+        const { pokemon } = this.props;
         return (
             <div className="pokedex">
-                <Pokemon key={pokemon.id} pokemon={pokemon} />)
+                <Pokemon key={pokemon.id} pokemon={pokemon} />
             </div>
         );
     }
